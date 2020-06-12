a64308167f902e87eb84ba34139449e5075887a1
yan zhou <yan.zhou@thoughtworks.com>
2020-06-12 21:03:01 +0800

    should_throw_Exception_when_store_package_and_have_full_locker :pear: @jiachen-zhou

--- src/main/java/tdd/Locker.java
+++ src/main/java/tdd/Locker.java
@@ -3,17 +3,22 @@ package tdd;
 public class Locker {
     private Ticket ticket;
     private boolean empty;
-
+    
     public Locker(Ticket ticket, boolean emptyFlag) {
         this.ticket = ticket;
         this.empty = emptyFlag;
     }
-
+    
     public boolean isEmpty() {
         return empty;
     }
-
+    
     public Ticket getTicket() {
         return this.ticket;
+    }
+    
+    public boolean store() throws LockerException {
+        if (empty) return empty;
+        else throw new LockerException("存包失败 提示储物柜已满");
     }
 }
---
+++ src/main/java/tdd/LockerException.java
@@ -0,0 +1,12 @@
+package tdd;
+
+/**
+ * @author Gavin
+ */
+public class LockerException extends Exception {
+    private static final long serialVersionUID = 5345800791722444L;
+    
+    public LockerException(String message) {
+        super(message);
+    }
+}
