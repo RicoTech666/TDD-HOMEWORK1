ade72c6c34e75eb995402592db7aec6bada76d82
yan zhou <yan.zhou@thoughtworks.com>
2020-06-18 22:51:46 +0800

    should_return_bag_when_SmartLockerRobot_get_package_given_valid_ticket:pear: @jia-chen-zhou

--- src/test/java/tdd/SmartRobotTest.java
+++ src/test/java/tdd/SmartRobotTest.java
@@ -14,9 +14,9 @@ public class SmartRobotTest {
     public void should_return_ticket_when_SmartLockerRobot_store_bag_given_two_lockers_have_capacity_and_one_bigger_than_another() throws LockerException {
         List<Locker> lockers = Arrays.asList(new Locker(1, 10, 5), new Locker(2, 8, 0));
         LockerRepo lockerRepo = new LockerRepo(lockers);
-        SmartLockerRobot primitiveLockerRobot = new SmartLockerRobot(lockerRepo);
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(lockerRepo);
         Bag bag = new Bag(1);
-        Ticket ticket = primitiveLockerRobot.storeBagBySmartLockerRobot(bag);
+        Ticket ticket = smartLockerRobot.storeBagBySmartLockerRobot(bag);
         
         Assert.assertTrue(ticket.getBagNumber() == lockers.get(1).getBag(ticket).getId());
     }
@@ -25,9 +25,9 @@ public class SmartRobotTest {
     public void should_return_ticket_and_store_in_1st_locker_when_SmartLockerRobot_store_bag_given_two_lockers_have_same_leftover_capacity() throws LockerException {
         List<Locker> lockers = Arrays.asList(new Locker(1, 10, 5), new Locker(2, 10, 5));
         LockerRepo lockerRepo = new LockerRepo(lockers);
-        SmartLockerRobot primitiveLockerRobot = new SmartLockerRobot(lockerRepo);
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(lockerRepo);
         Bag bag = new Bag(1);
-        Ticket ticket = primitiveLockerRobot.storeBagBySmartLockerRobot(bag);
+        Ticket ticket = smartLockerRobot.storeBagBySmartLockerRobot(bag);
         
         Assert.assertTrue(ticket.getBagNumber() == lockers.get(0).getBag(ticket).getId());
     }
@@ -36,10 +36,20 @@ public class SmartRobotTest {
     public void should_throw_exception_when_SmartLockerRobot_store_package_given_two_lockers_both_have_no_capacity() throws LockerException {
         List<Locker> lockers = Arrays.asList(new Locker(1, 10, 10), new Locker(2, 10, 10));
         LockerRepo lockerRepo = new LockerRepo(lockers);
-        SmartLockerRobot primitiveLockerRobot = new SmartLockerRobot(lockerRepo);
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(lockerRepo);
         Bag bag = new Bag(1);
         Assertions.assertThrows(LockerException.class, () -> {
-            primitiveLockerRobot.storeBagBySmartLockerRobot(bag);
+            smartLockerRobot.storeBagBySmartLockerRobot(bag);
         });
+    }
+    
+    @Test
+    public void should_return_bag_when_SmartLockerRobot_get_package_given_valid_ticket() throws LockerException {
+        List<Locker> lockers = Arrays.asList(new Locker(1, 10, 8), new Locker(2, 10, 10));
+        LockerRepo lockerRepo = new LockerRepo(lockers);
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(lockerRepo);
+        Bag bag = new Bag(1);
+        Ticket ticket = smartLockerRobot.storeBagBySmartLockerRobot(bag);
+        Assertions.assertTrue(bag.getId() == lockerRepo.getBag(ticket).getId());
     }
 }
