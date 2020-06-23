b9f1e3ca18673e638779782a62928f204beabed4
Jiachen Zhou <jiachen.zhou@rea-group.com>
2020-06-23 21:45:01 +0800

    should_get_bag_when_LockerRobotManager_fetch_bag_given_LockerRobotManager_has_one_locker_with_capacity_and_one_primaryLockerRobot_has_one_locker_with_capacity_and_one_smartLockerRobot_has_two_lockers_with_1st_has_capacity_2nd_has_no_capacity_with_valid_ticket :pear: @yan-zhou

--- src/test/java/tdd/LockerRobotManagerTest.java
+++ src/test/java/tdd/LockerRobotManagerTest.java
@@ -174,4 +174,26 @@ public class LockerRobotManagerTest {
 
         Assertions.assertEquals(bagStore.getId(), lockerRobotManager.getBag(ticket).getId());
     }
+
+    @Test
+    public void should_get_bag_when_LockerRobotManager_fetch_bag_given_LockerRobotManager_has_one_locker_with_capacity_and_one_primaryLockerRobot_has_one_locker_with_capacity_and_one_smartLockerRobot_has_two_lockers_with_1st_has_capacity_2nd_has_no_capacity_with_valid_ticket() throws LockerException {
+        List<Locker> primaryLockerRobotLockers = Arrays.asList(new Locker(1, 10, 8));
+        List<Locker> smartLockerRobotLockers = Arrays.asList(new Locker(1, 11, 10), new Locker(2, 10, 10));
+        List<Locker> lockerRobotManagerLockers = Arrays.asList(new Locker(1, 10, 10));
+        LockerRepo primaryLockerRobotLockerRepo = new LockerRepo(primaryLockerRobotLockers);
+        LockerRepo smartLockerRobotLockerRepo = new LockerRepo(smartLockerRobotLockers);
+        LockerRepo lockerRobotManagerLockerRepo = new LockerRepo(lockerRobotManagerLockers);
+
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(primaryLockerRobotLockerRepo);
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(smartLockerRobotLockerRepo);
+
+        List<LockerRobot> managedLockerRobots = Arrays.asList(primaryLockerRobot, smartLockerRobot);
+
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(managedLockerRobots, lockerRobotManagerLockerRepo);
+        Bag bagStore = new Bag(2);
+
+        Ticket ticket = lockerRobotManager.storeBag(bagStore);
+
+        Assertions.assertEquals(bagStore.getId(), lockerRobotManager.getBag(ticket).getId());
+    }
 }
